ca00751768a4e79d88890696be0aed8a02271c24
kangalioo <jannik.a.schaper@web.de>
2020-06-29 09:31:34 +0200

    Three things
    - More CanvasMessage ergonomics via four new method
    - New Canvas::toggle method
    - Canvas::{get, get_old, set} now don't panic anymore, but return None instead

--- src/canvas.rs
+++ src/canvas.rs
@@ -60,33 +60,89 @@ pub trait Canvas {
 	
 	// These are defaut implementations that you get for free
 
-	/// Sets the color at the given location. Panics if the location is out of bounds
-	fn set(&mut self, x: u32, y: u32, color: Color) {
-		if !self.is_valid(x, y) {
-			panic!("Coordinates ({}|{}) out of bounds", x, y);
+	/// Sets the color at the given location. Returns None if the location is out of bounds
+	fn set(&mut self, x: u32, y: u32, color: Color) -> Option<()> {
+		if self.is_valid(x, y) {
+			self.set_unchecked(x, y, color);
+			Some(())
+		} else {
+			None
 		}
-		self.set_unchecked(x, y, color);
 	}
 
-	/// Sets the color at the given location. Panics if the location is out of bounds
-	fn get(&self, x: u32, y: u32) -> Color {
-		if !self.is_valid(x, y) {
-			panic!("Coordinates ({}|{}) out of bounds", x, y);
+	/// Sets the color at the given location. Returns None if the location is out of bounds
+	fn get(&self, x: u32, y: u32) -> Option<Color> {
+		if self.is_valid(x, y) {
+			Some(self.get_unchecked(x, y))
+		} else {
+			None
 		}
-		return self.get_unchecked(x, y);
 	}
 
-	/// Retrieves the old, unflushed color at the given location. Panics if the location is out of
-	/// bounds
-	fn get_old(&self, x: u32, y: u32) -> Color {
-		if !self.is_valid(x, y) {
-			panic!("Coordinates ({}|{}) out of bounds", x, y);
+	/// Retrieves the old, unflushed color at the given location. Returns None if the location is
+	/// out of bounds
+	fn get_old(&self, x: u32, y: u32) -> Option<Color> {
+		if self.is_valid(x, y) {
+			Some(self.get_old_unchecked(x, y))
+		} else {
+			None
 		}
-		return self.get_old_unchecked(x, y);
 	}
 
+	/// An iterator over the buttons of a given Canvas. Create an iterator by calling `.iter()` on a
+	/// `Canvas`.
+	/// 
+	/// This iterator returns `CanvasButton`s, which are a view on a single button on the canvas. See
+	/// the documentation on `CanvasButton` for more information.
+	/// 
+	/// For example to light the entire canvas white:
+	/// ```rust
+	/// for button in canvas.iter() {
+	/// 	button.set(&mut canvas, Color::WHITE);
+	/// }
+	/// 
+	/// canvas.flush();
+	/// ```
+	/// 
+	/// Or, if you want to move the entire contents of the canvas one pixel to the right:
+	/// ```rust
+	/// for button in canvas.iter() {
+	/// 	let (x, y) = (button.x(), button.y());
+	/// 	if canvas.is_valid(x - 1, y) { // if there is a pixel to the left of this one
+	/// 		// Get the unflushed color from the left pixel and move it to this pixel
+	/// 		canvas.set(x, y, canvas.get_old(x - 1, y))
+	/// 	}
+	/// }
+	/// 
+	/// canvas.flush();
+	/// ```
 	fn iter(&self) -> CanvasIterator {
 		return CanvasIterator::new(self);
+	}
+
+	/// Toggles the button at the specified coordinate with the given color.
+	/// 
+	/// Light the specified button with the given color, except if the button is already lit with
+	/// that color; in which case the button is turned off.
+	/// 
+	/// For example, if you were to make a paint program for the Launchpad MK2 where you can toggle
+	/// pixels by pressing:
+	/// ```rust
+	/// let mut (canvas, poller) = launchy::mk2::Canvas::guess();
+	/// 
+	/// for msg in poller.iter() {
+	/// 	if let CanvasMessage::Press { x, y } = msg {
+	/// 		canvas.toggle(x, y, Color::WHITE);
+	/// 	}
+	/// }
+	/// ```
+	fn toggle(&mut self, x: u32, y: u32, color: Color) -> Option<()> {
+		if self.get(x, y)? == color {
+			self.set(x, y, Color::BLACK)?;
+		} else {
+			self.set(x, y, color)?;
+		}
+		Some(())
 	}
 }
 
@@ -126,30 +182,7 @@ impl CanvasButton {
 /// An iterator over the buttons of a given Canvas. Create an iterator by calling `.iter()` on a
 /// `Canvas`.
 /// 
-/// This iterator returns `CanvasButton`s, which are a view on a single button on the canvas. See
-/// the documentation on `CanvasButton` for more information.
-/// 
-/// For example to light the entire canvas white:
-/// ```rust
-/// for button in canvas.iter() {
-/// 	button.set(&mut canvas, Color::WHITE);
-/// }
-/// 
-/// canvas.flush();
-/// ```
-/// 
-/// Or, if you want to move the entire contents of the canvas one pixel to the right:
-/// ```rust
-/// for button in canvas.iter() {
-/// 	let (x, y) = (button.x(), button.y());
-/// 	if canvas.is_valid(x - 1, y) { // if there is a pixel to the left of this one
-/// 		// Get the unflushed color from the left pixel and move it to this pixel
-/// 		canvas.set(x, y, canvas.get_old(x - 1, y))
-/// 	}
-/// }
-/// 
-/// canvas.flush();
-/// ```
+/// For more information, see `Canvas::iter()`.
 pub struct CanvasIterator {
 	coordinates: Vec<(u32, u32)>, // the list of coordinates that we will iterate through
 	index: usize,
@@ -332,10 +365,47 @@ use std::collections::HashMap;
 /// 		CanvasMessage::Release { x, y } => println!("Released button at ({}|{})", x, y);
 /// 	}
 /// });
+/// ```
 #[derive(Clone, Eq, PartialEq, Hash, Debug)]
 pub enum CanvasMessage {
 	Press { x: u32, y: u32 },
 	Release { x: u32, y: u32 },
+}
+
+impl CanvasMessage {
+	/// Retrieves the x coordinate of this message, no matter if this is a press or a release
+	/// message
+	pub fn x(&self) -> u32 {
+		match *self {
+			Self::Press { x, y: _ } => x,
+			Self::Release { x, y: _ } => x,
+		}
+	}
+
+	/// Retrieves the y coordinate of this message, no matter if this is a press or a release
+	/// message
+	pub fn y(&self) -> u32 {
+		match *self {
+			Self::Press { x: _, y } => y,
+			Self::Release { x: _, y } => y,
+		}
+	}
+
+	/// Returns whether this is a press message
+	pub fn is_press(&self) -> bool {
+		match self {
+			Self::Press { .. } => true,
+			_ => false,
+		}
+	}
+
+	/// Returns whether this is a release message
+	pub fn is_release(&self) -> bool {
+		match self {
+			Self::Release { .. } => true,
+			_ => false,
+		}
+	}
 }
 
 struct LayoutDevice<'a> {
@@ -393,10 +463,14 @@ impl<'a> CanvasLayout<'a> {
 	}
 
 	/// Create a new CanvasLayout, plus an input handler object that you can use to poll messages.
-	pub fn new_polling() -> (CanvasLayoutPoller, Self) {
+	pub fn new_polling() -> (Self, CanvasLayoutPoller) {
 		let (sender, receiver) = std::sync::mpsc::sync_channel(50);
-		return (CanvasLayoutPoller { receiver }, Self::new(move |msg| sender.send(msg)
-				.expect("Message receiver has hung up (this shouldn't happen)")));
+		let canvas = Self::new(move |msg| sender.send(msg)
+				.expect("Message receiver has hung up (this shouldn't happen)"));
+		
+		let poller = CanvasLayoutPoller { receiver };
+
+		(canvas, poller)
 	}
 
 	/// Add a new device to this canvas layout, at the specified `x` and `y` coordinate.
